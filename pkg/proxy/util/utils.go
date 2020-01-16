64e34e072b157c6195653946ccc3b14901d05853
Jonathan Basseri <misterikkit@google.com>
2020-01-15 20:30:35 -0800

    Specify a DialContext in storage plugin clients
    The provided DialContext wraps existing clients' DialContext in an
    attempt to preserve any existing timeout configuration. In some cases,
    we may replace infinite timeouts with golang defaults.
    
    - glusterfs: tcp connect/keepalive values changed from 0/15 to 30/30 if using TLS
    - quobyte: no change
    - scaleio: tcp connect/keepalive values changed from 0/15 to 30/30
    - storageos: no change
    
    The added unit tests will fail to compile if the vendor patches are
    accidentally reverted.
    
    Tested: make test TESTS="./pkg/volume" GOFLAGS="-v -run=TestFork"
    Change-Id: I8d0b9b2855f91c40f314911ea6d05b5d86276c7d

--- pkg/proxy/util/utils.go
+++ pkg/proxy/util/utils.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/http"
 	"strconv"
 
 	v1 "k8s.io/api/core/v1"
@@ -310,4 +311,23 @@ func EnsureSysctl(sysctl utilsysctl.Interface, name string, newVal int) error {
 		klog.V(1).Infof("Changed sysctl %q: %d -> %d", name, oldVal, newVal)
 	}
 	return nil
+}
+
+// DialContext is a dial function matching the signature of net.Dialer.DialContext.
+type DialContext = func(context.Context, string, string) (net.Conn, error)
+
+// NewFilteredDialContext returns a DialContext function that only allows
+// connections to proxyable addresses, as defined by IsProxyableHostname. If the
+// wrapped DialContext is nil, all DNS and TCP settings use golang default
+// values.
+func NewFilteredDialContext(wrapped DialContext) DialContext {
+	if wrapped == nil {
+		wrapped = http.DefaultTransport.(*http.Transport).DialContext
+	}
+	return func(ctx context.Context, network, address string) (net.Conn, error) {
+		if err := IsProxyableHostname(ctx, net.DefaultResolver, address); err != nil {
+			return nil, err
+		}
+		return wrapped(ctx, network, address)
+	}
 }
