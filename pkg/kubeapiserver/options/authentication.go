cymtrick/kubernetes-error-fuzzer

default `service-account-extend-token-expiration` to true

pkg/kubeapiserver/options/authentication.go
@@ -165,7 +165,7 @@ func (o *BuiltInAuthenticationOptions) WithRequestHeader() *BuiltInAuthenticatio
 
 // WithServiceAccounts set default value for service account authentication
 func (o *BuiltInAuthenticationOptions) WithServiceAccounts() *BuiltInAuthenticationOptions {
-	o.ServiceAccounts = &ServiceAccountAuthenticationOptions{Lookup: true}
+	o.ServiceAccounts = &ServiceAccountAuthenticationOptions{Lookup: true, ExtendExpiration: true}
 	return o
 }
 

yuga711/kubernetes

BUG: 172499172
BUG: 172698276
(cherry picked from commit 4c593b268a24c32adde0939286370d46f5975f86)
Change-Id: Ia197d5eb5e3c6dd526407e3770b9fae2e3959013

pkg/kubeapiserver/options/authentication.go
@@ -144,7 +144,7 @@ func (s *BuiltInAuthenticationOptions) WithRequestHeader() *BuiltInAuthenticatio
 }
 
 func (s *BuiltInAuthenticationOptions) WithServiceAccounts() *BuiltInAuthenticationOptions {
-	s.ServiceAccounts = &ServiceAccountAuthenticationOptions{Lookup: true}
+	s.ServiceAccounts = &ServiceAccountAuthenticationOptions{Lookup: true, ExtendExpiration: true}
 	return s
 }
 